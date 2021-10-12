2a85e991253874af1c7360ba35f5f4350f47d337
Luiza100000 <92116715+Luiza100000@users.noreply.github.com>
2021-10-12 17:02:37 +0300

    Add files via upload

---
+++ hello_world.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+        public class hello_world {
+            public static void main(String[] args) {
+                Scanner in = new Scanner(System.in);
+                int s1 = in.nextInt();
+                int s2 = in.nextInt();
+                if (s1>s2){
+                    System.out.println(s1);
+                }
+                else{
+                    System.out.println(s2);
+                }
+            }
+        }
+
